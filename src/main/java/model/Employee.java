906f26283f2b571927fd2ded8c529c5a5542be64
Степан <www.niharaktera@yandex.ru>
2023-07-12 10:24:52 +0300

    создал entity и hibernate (класс + файл)

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,11 +1,24 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "employee")
+
 public class Employee {
-    private final int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+    @Column (name = "first_name")
     private String firstName;
+    @Column (name = "last_name")
     private String lastName;
+    @Column (name = "gender")
     private String gender;
+    @Column (name = "age")
     private int age;
+    @Column (name = "city_id")
     private int cityID;
 
     public Employee(int id, String firstName, String lastName, String gender, int age, int cityID) {
@@ -23,6 +36,9 @@ public class Employee {
         this.lastName = lastName;
         this.gender = gender;
         this.age = age;
+    }
+
+    public Employee() {
     }
 
     public int getId() {
